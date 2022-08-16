475826d7213857498c74a1b999f1394ac3f7b7d4
traviswertz <twertz@gmail.com>
2022-08-16 00:48:12 -0700

    initial

---
+++ blackjack.py
@@ -0,0 +1,11 @@
+class Card:
+
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+    def show_card(self):
+        print(f"{self.value} of {self.suit}")
+        
+
+
